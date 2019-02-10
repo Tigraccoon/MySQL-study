601857c53c4ccd452506751b4527166bc046b270
Tigraccoon <45990418+Tigraccoon@users.noreply.github.com>
2019-02-10 23:19:09 +0900

    20190210

--- project2/member.sql
+++ project2/member.sql
@@ -7,6 +7,7 @@ NAME VARCHAR(50) NOT NULL,
 hp VARCHAR(50) NOT NULL,
 address VARCHAR(150) NOT NULL,
 email VARCHAR(100) NOT NULL,
+amount INT DEFAULT 0,
 POINT INT DEFAULT 0,
 DATE DATE,
 admin INT DEFAULT 0
@@ -18,5 +19,5 @@ SELECT * FROM member;
 
 DESC member;
 
-INSERT INTO member VALUES('admin','admin','관리자','000-0000-0000','서울','admin@admin.com',9999999,NOW(),1);
-INSERT INTO member VALUES('user1','1111','홍길동','010-1111-1111','서울시 강남구 서초동','user1@user.com',0,NOW(),0);
+INSERT INTO member VALUES('admin','admin','관리자','000-0000-0000','서울','admin@admin.com',9999999,99999999,NOW(),1);
+INSERT INTO member VALUES('user1','1111','홍길동','010-1111-1111','서울시 강남구 서초동','user1@user.com',0,50000,NOW(),0);
