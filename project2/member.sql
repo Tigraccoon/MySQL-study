c9892852eed44eb5e74a4bb4cc822aef326fe8eb
Tigraccoon <45990418+Tigraccoon@users.noreply.github.com>
2019-02-11 23:34:17 +0900

    20190211

--- project2/member.sql
+++ project2/member.sql
@@ -1,5 +1,6 @@
 USE project2
 
+--회원 테이블
 CREATE TABLE member(
 id VARCHAR(100) NOT NULL PRIMARY KEY,
 pwd VARCHAR(100) NOT NULL,
@@ -24,3 +25,27 @@ INSERT INTO member VALUES('user1','1111','홍길동','010-1111-1111','서울시 
 SELECT id,pwd,NAME,hp,address,email,amount,POINT,DATE FROM member ORDER BY id;
 
 SELECT id,pwd FROM member WHERE email LIKE 'admin@admin.com';
+
+
+
+--상품 테이블
+CREATE TABLE product(
+pcode INT NOT NULL AUTO_INCREMENT,
+pname VARCHAR(50) NOT NULL,
+price INT,
+pamount INT,
+PRIMARY KEY(pcode)
+);
+
+DESC product;
+
+INSERT INTO product (pname,price,pamount) VALUES('JavaEE 1year serial number',999,500);
+INSERT INTO product (pname,price,pamount) VALUES('Oracle DB 1year serial number',499,500);
+INSERT INTO product (pname,price,pamount) VALUES('MSSQL 1year serial number',599,500);
+INSERT INTO product (pname,price,pamount) VALUES('Photoshop CC 2019 serial number',399,500);
+INSERT INTO product (pname,price,pamount) VALUES('Premiere Pro CC 2019 serial number',799,500);
+INSERT INTO product (pname,price,pamount) VALUES('TeamViewer 1year serial number',299,500);
+INSERT INTO product (pname,price,pamount) VALUES('GitHUB 1year serial number',99,500);
+INSERT INTO product (pname,price,pamount) VALUES('Norton Security 1year serial number',199,500);
+
+SELECT * FROM product;
